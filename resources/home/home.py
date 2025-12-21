d3b931f8be444ca34bc9fc600f1d99279904b92d
Jeerapat Sripumngoen <jeerapatsepu@gmail.com>
2025-12-21 21:09:57 +0700

    add home service

--- resources/home/home.py
+++ resources/home/home.py
@@ -1,20 +1,52 @@
 # import logging
-from flask import request as dd, render_template
+import uuid
 from flask.views import MethodView
 from flask_smorest import Blueprint
-from flask_jwt_extended import (
-    create_access_token,
-    create_refresh_token
-)
+from flask_jwt_extended import jwt_required
 from datetime import datetime, timezone
-from models.usli import USLI
-from app.shared import uid, bcrypt
-from schemas.reponse_schema.error import ErrorSchema
+from models.post.post import Post
+from resources.base.short_post import ShortPost
+from resources.profile.post.profile_posts_response import ProfilePostsResponseSchema
 from schemas.reponse_schema.meta import MetaSchema
+from schemas.request_schema.profile.profile_posts_request_schema import ProfilePostsRequestSchema
 
 blp = Blueprint("Home", __name__, description="Home")
 
-@blp.route("/")
+@blp.route("/home/feed")
 class Home(MethodView):
+    @jwt_required(optional=True)
+    @blp.arguments(ProfilePostsRequestSchema)
+    @blp.response(200, ProfilePostsResponseSchema)
     def post(self, request):
-        return "ii"
+        uid = request["uid"]
+        offset = request["offset"]
+        limit = request["limit"]
+        posts = Post.query.order_by(Post.created_date_timestamp).offset(offset).limit(limit).all()
+        posts.sort(key=self.__sortPostsList, reverse=True)
+        new_posts = self.__getShortPost(posts=posts)
+        return self.__getPofilePostsSuccessResponse(new_posts)
+
+    def __getShortPost(self, posts: list):
+        short_post_list = []
+        for post in posts:
+            short_post = ShortPost(post_id=post.post_id).get_post()
+            short_post_list.append(short_post)
+        return short_post_list
+
+    def __sortPostsList(self, e):
+        return e.created_date_timestamp
+
+    def __getPofilePostsSuccessResponse(self, posts):
+        time = datetime.now(timezone.utc)
+
+        meta = MetaSchema()
+        meta.response_id = uuid.uuid4().hex
+        meta.response_code = 1000
+        meta.response_date = str(time)
+        meta.response_timestamp = str(time.timestamp())
+        meta.error = None
+
+        response = ProfilePostsResponseSchema()
+        response.meta = meta
+        response.data = posts
+        return response
