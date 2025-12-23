bcd89bff3f3bfd84f544005ae0000f093e0d245e
Jeerapat Sripumngoen <jeerapatsepu@gmail.com>
2025-12-23 09:48:09 +0700

    add filter profile posts

--- resources/profile/post/profile_posts.py
+++ resources/profile/post/profile_posts.py
@@ -4,6 +4,8 @@ from flask_smorest import Blueprint
 from flask_jwt_extended import jwt_required
 from datetime import datetime, timezone
 from models.post.post import Post
+from models.post.post_bookmark_model import PostBookmarkModel
+from models.post.post_repost_model import PostRepostModel
 from resources.base.short_post import ShortPost
 from resources.profile.post.profile_posts_response import ProfilePostsResponseSchema
 from schemas.reponse_schema.meta import MetaSchema
@@ -22,12 +24,40 @@ class ProfilePosts(MethodView):
         uid = request["uid"]
         offset = request["offset"]
         limit = request["limit"]
-        posts = Post.query.order_by(Post.created_date_timestamp).filter(Post.owner_uid==uid, Post.visibility=="PUBLIC").offset(offset).limit(limit).all()
-        posts.sort(key=self.__sortPostsList, reverse=True)
-        new_posts = self.__getShortPost(posts=posts)
-        return self.__getPofilePostsSuccessResponse(new_posts)
+        filter = request["filter"]
+        posts = []
+        match filter:
+            case "ALL":
+                posts = Post.query.order_by(Post.created_date_timestamp).filter(Post.owner_uid==uid, Post.visibility=="PUBLIC").offset(offset).limit(limit).all()
+                posts.sort(key=self.__sortPostsList, reverse=True)
+                posts = self.__getShortPost(posts=posts)
+            case "REPOSTS":
+                reposts = PostRepostModel.query.filter_by(user_uid=uid).order_by(PostRepostModel.created_date_timestamp).offset(offset).limit(limit).all()
+                reposts.sort(key=self.__sortPostsList, reverse=True)
+                posts = self.__getShortPost(posts=reposts)
+            case "BOOKMARKS":
+                bookmarks = PostBookmarkModel.query.filter_by(user_uid=uid).order_by(PostBookmarkModel.created_date_timestamp).offset(offset).limit(limit).all()
+                bookmarks.sort(key=self.__sortPostsList, reverse=True)
+                posts = self.__getShortPost(posts=bookmarks)
+            case "PRIVATE":
+                posts = Post.query.order_by(Post.created_date_timestamp).filter(Post.owner_uid==uid, Post.visibility=="PRIVATE").offset(offset).limit(limit).all()
+                posts.sort(key=self.__sortPostsList, reverse=True)
+                posts = self.__getPrivateShortPost(posts=posts)
+            case _:
+                posts = Post.query.order_by(Post.created_date_timestamp).filter(Post.owner_uid==uid, Post.visibility=="PUBLIC").offset(offset).limit(limit).all()
+                posts.sort(key=self.__sortPostsList, reverse=True)
+                posts = self.__getShortPost(posts=posts)
+        return self.__getPofilePostsSuccessResponse(posts)
 
     def __getShortPost(self, posts: list):
+        short_post_list = []
+        for post in posts:
+            short_post = ShortPost(post_id=post.post_id).get_post()
+            if short_post.visibility != "PRIVATE":
+                short_post_list.append(short_post)
+        return short_post_list
+    
+    def __getPrivateShortPost(self, posts: list):
         short_post_list = []
         for post in posts:
             short_post = ShortPost(post_id=post.post_id).get_post()
--- schemas/request_schema/profile/profile_posts_request_schema.py
+++ schemas/request_schema/profile/profile_posts_request_schema.py
@@ -4,3 +4,4 @@ class ProfilePostsRequestSchema(Schema):
     uid = fields.Str(required=True, validate=validate.Length(min=1))
     offset = fields.Int(required=True)
     limit = fields.Int(required=True)
+    filter = fields.Str(validate=validate.OneOf(["ALL", "REPOSTS", "BOOKMARKS", "PRIVATE"]), required=False)
