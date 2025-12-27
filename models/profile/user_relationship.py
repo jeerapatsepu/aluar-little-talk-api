96bba53b9ce1c8d29af16c2d1dac1f1e03bd4f99
Jeerapat Sripumngoen <jeerapatsepu@gmail.com>
2025-12-27 20:02:31 +0700

    fix database relationship

--- models/profile/user_relationship.py
+++ models/profile/user_relationship.py
@@ -4,7 +4,7 @@ from app.shared import db
 class UserRelationship(db.Model):
     __tablename__ = "user_relationships"
 
-    id = db.Column(db.String, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     sender_id = db.Column(db.String)
     receiver_id = db.Column(db.String)
     status = db.Column(db.String) # e.g., 'FOLLOW', 'BLOCKED', 'FRIEND'
