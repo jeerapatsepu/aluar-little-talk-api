7f0b48783f1e59afa78181c28e77a60cdb605987
Jeerapat Sripumngoen <jeerapatsepu@gmail.com>
2026-01-03 19:51:59 +0700

    add service change profile

--- resources/profile/profile_change_photo/profile_change_photo_schema.py
+++ resources/profile/profile_change_photo/profile_change_photo_schema.py
@@ -8,4 +8,4 @@ class ProfileChangePhotoRequestSchema(Schema):
 
 class ProfileChangePhotoResponseSchema(Schema):
     meta = fields.Nested(MetaSchema, only=("response_id", "response_code", "response_date", "response_timestamp", "error"), dump_only=True)
-    data = fields.Nested(ProfileDataResponseSchema, only=("name", "uid", "photo", "caption", "link", "email"), dump_only=True, many=True)
+    data = fields.Nested(ProfileDataResponseSchema, only=("name", "uid", "photo", "caption", "link", "email"), dump_only=True, many=False)
